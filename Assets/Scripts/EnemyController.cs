3dbc61f9cd1eef59c8976a874ddbe10bab74ed95
Michelle Quiroz <388775@colum.edu>
2020-10-22 00:41:48 -0500

    Ruby running animation, cog projectiles, Robot moving and fixed animations
    Ruby is now able to look like she's running in different directions when she moves, so she's no longer stuck on one sprite. I also added  the cog projectile to Ruby and ensured that it doesn't collide with Ruby. The Robot enemy also has movement animations, and when hit by Ruby's cog projectile, they dance to signify they are fixed. Had to modify the Robot's collider to ensure it would be hit by the cog.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,15 +12,26 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
+    bool broken = true;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -28,22 +39,38 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
+
+       
+
     }
 
     void FixedUpdate()
     {
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction; ;
+
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction; ;
+            position.x = position.x + Time.deltaTime * speed * direction;
+
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
+
+        
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -54,5 +81,13 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+
+        animator.SetTrigger("Fixed");
     }
 }
